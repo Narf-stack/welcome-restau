920ab734bf45b3ab13122b75252178cb84c82a36
Frantz Crozilhac <fran.stacksta@gmail.com>
2021-04-24 10:24:04 +0200

    set up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:home, :show]
+
   def home
+  end
+  def show
+  end
+  def add_to_basket
   end
 end
